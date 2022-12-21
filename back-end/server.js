6b519294fc0c1b730466b50af8068034b2f4275f
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-12-21 16:32:58 -0500

    Preparing for deployment

--- back-end/server.js
+++ back-end/server.js
@@ -1,23 +1,36 @@
 const express = require("express");
-const notes = require("./data/notes");
+// const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoute");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-const { protect } = require("./middleware/authMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
-
 app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
+
+// --------------- Deployment --------------- //
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/front-end/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------- Deployment --------------- //
 
 // Error Handling Middlewares
 app.use(notFound);
